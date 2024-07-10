be6df853de4df216ea1fe9aab2d049bcefdb7800
Ali-Alzyoud <ali198724@gmail.com>
2024-07-10 20:11:39 +0300

    Add files via upload

--- routes/question.js
+++ routes/question.js
@@ -22,7 +22,7 @@ function getAllItems(){
   return items;
 }
 
-function updateKeyword(Keyword){
+function updateKeyword(Keyword, reviewer){
   const data = fs.readFileSync("questions.json", "utf8");
   const questions = JSON.parse(data.replace(/(\r\n|\n|\r|\t)/gm, ""));
   let founded = false;
@@ -41,6 +41,11 @@ function updateKeyword(Keyword){
     return q;
   });
   fs.writeFileSync("questions.json",JSON.stringify(q2));
+
+  const reviewedData = fs.readFileSync("reviewed.json", "utf8");
+  const reviewed = JSON.parse(reviewedData.replace(/(\r\n|\n|\r|\t)/gm, ""));
+  reviewed[Keyword.Id] = reviewer || "anonymous";
+  fs.writeFileSync("reviewed.json",JSON.stringify(reviewed));
   return founded;
 }
 
@@ -50,15 +55,19 @@ router.get("/count", function (req, res, next) {
 });
 
 router.get("/:number", function (req, res, next) {
-  const { number } = req.params;
+  let { number } = req.params;
+  number = Number(number);
   const items = getAllItems();
-  res.json(items[number - 1]);
+
+  const reviewedData = fs.readFileSync("reviewed.json", "utf8");
+  const reviewed = JSON.parse(reviewedData.replace(/(\r\n|\n|\r|\t)/gm, ""));
+  res.json({question:items?.[number - 1], reviewed:reviewed?.[items?.[number - 1]?.Id] || ""});
 });
 
 router.post("/", function (req, res, next) {
-  const Keyword = req.body;
-  if(Keyword?.Id){
-    updateKeyword(Keyword);
+  const {question, reviewed} = req.body;
+  if(question?.Id){
+    updateKeyword(question, reviewed);
   }
   res.json({success:true});
 });
